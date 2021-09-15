cc0e7f7b426586d05f7d608ec446120c5ecf030c
parthmodi444 <88581955+parthmodi444@users.noreply.github.com>
2021-09-16 00:21:10 +0530

    Some changes

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
 const billAmount=document.querySelector("#bill-amount");
-const cashGiven=document.querySelector("#cash-given");
-const checkButton=document.querySelector("#check-button");
-var message=document.querySelector('#error-msg');
-alert(billAmount);
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-msg");
+var noOfNotes=document.querySelectorAll(".no-of-notes");
+const availableNotes=[2000,500,100,20,10,5,1];
+function showMessage(msg)
+{
+    message.style.display="block";
+    message.innerHTML=msg;
+}
+function calculateChange(amountToBeReturn)
+{
+for(var i=0;i<availableNotes.length;i=i+1)
+{
+
+    const numberOfNotes=Math.trunc(amountToBeReturn/availableNotes[i]);
+    amountToBeReturn=amountToBeReturn % availableNotes[i];
+    noOfNotes[i].innerHTML=numberOfNotes;
+ 
+
+}
+
+}
 checkButton.addEventListener("click",function validateBillandCashAmount()
 {
-if(billAmount.value>0)
-{
-    if(cashGiven>=billAmount)
+
+    message.style.display="none";
+    if(isNaN(billAmount.value) || isNaN(cashGiven.value) )
     {
+        showMessage("It cannot be String");
+    }
+    else if(billAmount.value >0)    
+    {
+        if(cashGiven.value >= billAmount.value )
+        {
+            alert("hii seema");
+            var amountToBeReturned=cashGiven.value-billAmount.value;
+            calculateChange(amountToBeReturned);
+
+
+        }
+
+        else
+        {
+
+            showMessage("Do u wanna wash plates");
+        }
+
 
     }
     else
     {
-        message("Do u wanna wash utensils");
+        showMessage("Invalid bill amount");
     }
 }
-else{
-message("Invalid bill amount")
+);
 
 
-}
-
-
-}
-
-)
-function message(msg)
-{
-message.innerHTML = msg;
-
-}
